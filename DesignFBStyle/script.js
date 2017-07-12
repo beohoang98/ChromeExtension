94d6a93dd42cba30e6336e07a50650ec2431d59a
Beo Hoang <beohoang98@users.noreply.github.com>
2017-07-12 22:52:29 +0700

    add FBDesignDisplay

--- DesignFBStyle/script.js
+++ DesignFBStyle/script.js
@@ -1,4 +1,5 @@
 var styleAdd = document.createElement('link');
 styleAdd.rel = "stylesheet";
+styleAdd.id = "link-css";
 styleAdd.href = chrome.extension.getURL('style.css');
 document.body.prepend(styleAdd);
