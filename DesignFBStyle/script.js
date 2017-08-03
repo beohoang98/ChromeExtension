18eaa8813e2df9a3852d215af282d576f899587a
Beo Hoang <beohoang98@users.noreply.github.com>
2017-08-03 10:34:20 +0700

    change style to -Card in the wood wall-

--- DesignFBStyle/script.js
+++ DesignFBStyle/script.js
@@ -3,3 +3,21 @@ styleAdd.rel = "stylesheet";
 styleAdd.id = "link-css";
 styleAdd.href = chrome.extension.getURL('style.css');
 document.body.prepend(styleAdd);
+var urlBG = chrome.extension.getURL("background.jpg");
+// urlBG = "background.png";
+document.body.style.backgroundImage = "url(\"" + urlBG + "\")";
+
+//smooth scroll
+document.querySelector("#globalContainer").onmousewheel = smoothWheel;
+
+function smoothWheel(event) {
+    event.preventDefault();
+
+    var delta = event.wheelDelta / 120 || -event.detail / 3;
+
+    window.scrollBy({
+        top: -delta*400,
+        left: 0,
+        behavior: "smooth"
+    })
+}
